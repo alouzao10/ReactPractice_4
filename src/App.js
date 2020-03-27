a895d5c9313351af6c45c42b528c7547229582ab
alouzao10 <alouzao10@gmail.com>
2020-03-27 13:06:57 -0400

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function ToDo({ todo, index }) {
-  return <div className='todo'>{todo.text}</div>;
+function ToDo({ todo, index, setComplete, deleteToDo }) {
+  return (
+    <div
+      style={{ textDecoration: todo.isComplete ? 'line-through' : '' }}
+      className='todo'
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => setComplete(index)}>Toggle Task</button>
+        <button onClick={() => deleteToDo(index)}>Remove Task</button>
+      </div>
+    </div>
+  );
 }
 
 function ToDoForm({ addToDo }) {
@@ -35,7 +46,7 @@ function App() {
   const [todo, setToDo] = useState([
     {
       text: 'Learn about React...',
-      isComplete: false
+      isComplete: true
     },
     {
       text: 'Learn more about React...',
@@ -52,11 +63,34 @@ function App() {
     setToDo(newToDo);
   };
 
+  const setComplete = index => {
+    const newToDo = [...todo];
+
+    if (newToDo[index].isComplete) {
+      newToDo[index].isComplete = false;
+    } else {
+      newToDo[index].isComplete = true;
+    }
+    setToDo(newToDo);
+  };
+
+  const deleteToDo = index => {
+    const newToDo = [...todo];
+    newToDo.splice(index, 1);
+    setToDo(newToDo);
+  };
+
   return (
     <div className='app'>
       <div className='todo-list'>
         {todo.map((item, index) => (
-          <ToDo key={index} index={index} todo={item} />
+          <ToDo
+            key={index}
+            index={index}
+            todo={item}
+            setComplete={setComplete}
+            deleteToDo={deleteToDo}
+          />
         ))}
         <ToDoForm addToDo={addToDo} />
       </div>
